7d4745ff977613a61ef8cb523dfbd0623f4b30ec
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-07-22 16:35:17 +0700

    update firebase project configuration

--- src/js/utils/firebase.js
+++ src/js/utils/firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDb4Dc0DdfUvK4RNebVW5ckLCSyTfr9Oc8',
-  authDomain: 'storyapp-409f8.firebaseapp.com',
-  projectId: 'storyapp-409f8',
-  storageBucket: 'storyapp-409f8.appspot.com',
-  messagingSenderId: '688766931446',
-  appId: '1:688766931446:web:cbc56bb250ab3f312277d7',
-  measurementId: 'G-DE1LJNEG1T',
+  apiKey: "AIzaSyDgdsXHB_AHtW78IofqHYHEwvkEEo_gA9o",
+  authDomain: "bagicerita-220724.firebaseapp.com",
+  projectId: "bagicerita-220724",
+  storageBucket: "bagicerita-220724.appspot.com",
+  messagingSenderId: "429337246912",
+  appId: "1:429337246912:web:35c813774c6026b05bc125",
+  measurementId: "G-F19Q7ZXHRR",
 };
 
 // Initialize Firebase
